7fbb5de4f9997084b78c86f56faa35c59c7d8f19
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-15 03:28:09 +0530

    number type

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,16 @@
-console.log("Hello, World!!!")
+// 01. printing "Hello, World!!!"
+// console.log("Hello, World!!!")
+
+// Primitive Types
+
+// number type
+
+// let pizzaCost: number = 10;
+const pizzaCost: number = 10;
+const pizzaToppings: number = 2;
+
+function calculatePrice(cost: number, toppings: number): number {
+  return cost + 2 * toppings;
+}
+
+const cost: number = calculatePrice(pizzaCost, pizzaToppings);
