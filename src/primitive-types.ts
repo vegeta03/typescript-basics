911c8335e23bb8fc292e9b607e072ad93573b870
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-15 13:40:41 +0530

    primitive-types.ts

---
+++ src/primitive-types.ts
@@ -0,0 +1,36 @@
+// printing "Hello, World!!!"
+console.log('Hello, World!!!');
+
+let pizzaCost: number = 10;
+const pizzaToppings: number = 2;
+
+// string type and literals
+
+let coupon: string;
+coupon = 'pizza25';
+
+function normalizeCoupon(code: string): string {
+  return code.toUpperCase();
+}
+
+const code = normalizeCoupon(coupon);
+console.log(code);
+
+const multiLineString = `
+This is a 
+multi-line string.
+`;
+console.log(multiLineString);
+
+let couponMesage = normalizeCoupon(coupon);
+// ES6 string literal
+couponMesage = `The Final coupon code is ${couponMesage}`;
+console.log(couponMesage);
+
+// boolean
+
+let isTrue: boolean;
+isTrue = true;
+console.log(isTrue);
+isTrue = !isTrue;
+console.log(isTrue);
