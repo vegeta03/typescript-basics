004308f6b5b15d78240eaa74a0210fca1ffad223
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-15 20:37:23 +0530

    type aliases

---
+++ src/type-aliases-assertions.ts
@@ -0,0 +1,12 @@
+// // type aliases
+// let pizzaSize: 'small' | 'medium' | 'large';
+// const selectSize = (size: 'small' | 'medium' | 'large') => (pizzaSize = size);
+// selectSize('small');
+
+// type Size = 'small' | 'medium' | 'large';
+// type callback = (size: Size) => void;
+// pizzaSize = 'small';
+// const selectPizzaSize: callback = (x) => (pizzaSize = x);
+// selectPizzaSize('medium');
+
+// type assertions
