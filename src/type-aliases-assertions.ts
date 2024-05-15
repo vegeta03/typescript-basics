82eeada00e0318d6c710b2f9074715483dbf76b4
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-15 20:50:14 +0530

    type assertions

--- src/type-aliases-assertions.ts
+++ src/type-aliases-assertions.ts
@@ -9,4 +9,17 @@
 // const selectPizzaSize: callback = (x) => (pizzaSize = x);
 // selectPizzaSize('medium');
 
-// type assertions
+// // type assertions
+// type Pizza = { name: string; toppings: number };
+// const pizza: Pizza = { name: 'Blazing Inferno', toppings: 5 };
+
+// const serialized = JSON.stringify(pizza);
+
+// function getNameFromJSON(obj: string) {
+//   // return JSON.parse(obj);
+//   // return (<Pizza>JSON.parse(obj)).name;
+//   return (JSON.parse(obj) as Pizza).name;
+// }
+
+// const pizzaName = getNameFromJSON(serialized);
+// console.log(pizzaName);
