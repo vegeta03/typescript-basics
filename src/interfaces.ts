a7fb86a3a082a4a5023e62b4ec8450436fc13f57
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-15 21:14:56 +0530

    extending interfaces

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -38,3 +38,26 @@
 // pizza.getAvailableSizes();
 
 // extending interfaces
+interface Sizes {
+  sizes: string[];
+}
+
+interface Pizza extends Sizes {
+  name: string;
+  getAvailableSizes(): string[];
+}
+
+let pizza: Pizza;
+
+function createPizza(name: string, sizes: string[]): Pizza {
+  return {
+    name,
+    sizes,
+    getAvailableSizes() {
+      return this.sizes;
+    },
+  };
+}
+
+pizza = createPizza('Pepperoni', ['small', 'medium']);
+pizza.getAvailableSizes();
