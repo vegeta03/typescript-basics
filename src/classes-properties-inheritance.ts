df8e11bc24a9bb0ce0487671d85559ade87fb62a
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-16 01:29:33 +0530

    setters and getters

--- src/classes-properties-inheritance.ts
+++ src/classes-properties-inheritance.ts
@@ -1,18 +1,76 @@
-// classes, properties and inheritance
-class Pizza {
-  name: string;
-  toppings: string[] = [];
+// // understanding classes and constructors
+// class Pizza {
+//   name: string;
+//   toppings: string[] = [];
 
-  constructor(name: string) {
-    this.name = name;
+//   constructor(name: string) {
+//     this.name = name;
+//   }
+
+//   addToppings(topping: string): void {
+//     this.toppings.push(topping);
+//   }
+// }
+
+// const pizza = new Pizza('Pepperoni');
+// pizza.addToppings('pepperoni');
+
+// console.log(pizza);
+
+// // public and private members
+// class Pizza {
+//   toppings: string[] = [];
+
+//   constructor(private name: string) {}
+
+//   addToppings(topping: string): void {
+//     this.toppings.push(topping);
+//   }
+
+//   getName() {
+//     return this.name;
+//   }
+// }
+
+// const pizza = new Pizza('Pepperoni');
+// pizza.addToppings('pepperoni');
+
+// console.log(pizza);
+
+// // readonly members
+// class Pizza {
+//     toppings: string[] = [];
+
+//     constructor(readonly name: string) {}
+
+//     addToppings(topping: string): void {
+//       this.toppings.push(topping);
+//     }
+
+//     getName() {
+//       return this.name;
+//     }
+//   }
+
+//   const pizza = new Pizza('Pepperoni');
+//   pizza.addToppings('pepperoni');
+
+//   console.log(pizza);
+
+// setters and getters
+class Sizes {
+  constructor(private sizes: Array<string>) {}
+
+  get availableSizes() {
+    return this.sizes;
   }
 
-  addToppings(topping: string): void {
-    this.toppings.push(topping);
+  set availableSizes(sizes: Array<string>) {
+    this.sizes = sizes;
   }
 }
 
-const pizza = new Pizza('Pepperoni');
-pizza.addToppings('pepperoni');
-
-console.log(pizza);
+const sizes = new Sizes(['small', 'medium']);
+console.log(sizes.availableSizes);
+sizes.availableSizes = ['medium', 'large'];
+console.log(sizes.availableSizes);
