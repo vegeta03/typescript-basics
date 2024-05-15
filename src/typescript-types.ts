41776ad3964e339cfb84531af4b1a161030d9509
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-15 14:25:19 +0530

    null, undefined, strict null checks

--- src/typescript-types.ts
+++ src/typescript-types.ts
@@ -1,19 +1,42 @@
-// `any` type
-let coupon;
-coupon = 25;
-coupon = 'pizza25';
-coupon = true;
+// // `any` type
+// let coupon;
+// coupon = 25;
+// coupon = 'pizza25';
+// coupon = true;
 
-// Implicit vs Explicit types
-let implicitCoupon = 'pizza25';
-let explicitCoupon: string = 'pizza25';
+// // Implicit vs Explicit types
+// let implicitCoupon = 'pizza25';
+// let explicitCoupon: string = 'pizza25';
 
-// `void` type
-// `void` type does not exist in JS, it is opposite to the `any` type
-let selectedTopping: string = 'pepperoni';
+// // `void` type
+// // `void` type does not exist in JS, it is opposite to the `any` type
+// let selectedTopping: string = 'pepperoni';
 
-function selectTopping(topping: string): void {
-  selectedTopping = topping;
+// function selectTopping(topping: string): void {
+//   selectedTopping = topping;
+// }
+
+// console.log(`The selected topping is ${selectedTopping}`);
+
+// // `never` type
+// function orderError(error: string): never {
+//   throw new Error(error);
+//   console.log('This is never printed');
+//   // never going to return a value
+// }
+
+// orderError('Something went wrong');
+
+// null, undefined, strict null checks
+let coupon: string | null = 'pizza25';
+
+function removeCoupon() {
+  coupon = null;
 }
 
-console.log(`The selected topping is ${selectedTopping}`);
+console.log(coupon);
+removeCoupon();
+console.log(coupon);
+
+//  union and literal types
+
