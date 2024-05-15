0e5211364c9bd8bc81fe389a422e24a92f781c0a
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-16 01:09:39 +0530

    classes, properties

--- src/classes-properties-inheritance.ts
+++ src/classes-properties-inheritance.ts
@@ -1,1 +1,18 @@
-// classes, properties and inheritance 
+// classes, properties and inheritance
+class Pizza {
+  name: string;
+  toppings: string[] = [];
+
+  constructor(name: string) {
+    this.name = name;
+  }
+
+  addToppings(topping: string): void {
+    this.toppings.push(topping);
+  }
+}
+
+const pizza = new Pizza('Pepperoni');
+pizza.addToppings('pepperoni');
+
+console.log(pizza);
