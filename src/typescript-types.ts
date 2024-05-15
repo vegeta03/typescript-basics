ff82bcf105df522f8077361252dccb8938f62b5c
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-15 14:05:46 +0530

    `void` type

--- src/typescript-types.ts
+++ src/typescript-types.ts
@@ -5,3 +5,15 @@ coupon = 'pizza25';
 coupon = true;
 
 // Implicit vs Explicit types
+let implicitCoupon = 'pizza25';
+let explicitCoupon: string = 'pizza25';
+
+// `void` type
+// `void` type does not exist in JS, it is opposite to the `any` type
+let selectedTopping: string = 'pepperoni';
+
+function selectTopping(topping: string): void {
+  selectedTopping = topping;
+}
+
+console.log(`The selected topping is ${selectedTopping}`);
