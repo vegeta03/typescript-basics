3a5b3a9a2e0c89a2be92cc8580a225b0991ab8f3
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-15 16:40:12 +0530

    functions and optional arguments

--- src/typescript-types.ts
+++ src/typescript-types.ts
@@ -48,20 +48,44 @@
 // selectPizzaSize('medium');
 // console.log(`Pizza size is ${pizzaSize}`);
 
-// `function` types
-function sumOrder(price: number, quantity: number): number {
-  return price * quantity;
-}
+// // `function` types
+// function sumOrder(price: number, quantity: number): number {
+//   return price * quantity;
+// }
 
-const total = sumOrder(25, 4);
-console.log(`total: ${total}`);
+// const total = sumOrder(25, 4);
+// console.log(`total: ${total}`);
 
-const sumOrder1 = (price: number, quantity: number) => price * quantity;
-const total1 = sumOrder1(25, 1);
-console.log(`total1: ${total1}`);
+// const sumOrder1 = (price: number, quantity: number) => price * quantity;
+// const total1 = sumOrder1(25, 1);
+// console.log(`total1: ${total1}`);
 
-// let sumOrder2: Function;
-let sumOrder2: (price: number, quantity: number) => number;
-sumOrder2 = (x, y) => x * y;
-const total2 = sumOrder2(25, 2);
-console.log(`total2: ${total2}`);
+// // let sumOrder2: Function;
+// let sumOrder2: (price: number, quantity: number) => number;
+// sumOrder2 = (x, y) => x * y;
+// const total2 = sumOrder2(25, 2);
+// console.log(`total2: ${total2}`);
+
+// // functions and optional arguments
+// let sumOrder: (price: number, quantity?: number) => number;
+// sumOrder = (x, y) => {
+//   if (y) {
+//     return x * y;
+//   } else {
+//     return x;
+//   }
+// };
+
+// let total = sumOrder(25);
+// console.log(`sumOrder(25) is ${total}`);
+// total = sumOrder(25, 2);
+// console.log(`sumOrder(25, 2) is ${total}`);
+
+// sumOrder = (x, y) => (y ? x * y : x);
+
+// total = sumOrder(25);
+// console.log(`sumOrder(25) is ${total}`);
+// total = sumOrder(25, 2);
+// console.log(`sumOrder(25, 2) is ${total}`);
+
+// typed functions and default parameters
