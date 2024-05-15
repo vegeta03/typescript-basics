9aa460ea70afe1ee25b2e17905d4c7898ccb17a5
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-15 21:11:27 +0530

    interfaces with function types

--- src/interfaces.ts
+++ src/interfaces.ts
@@ -14,3 +14,27 @@
 // }
 
 // pizza = createPizza('Pepperoni', ['small', 'medium']);
+
+// // interfaces with function types
+// interface Pizza {
+//   name: string;
+//   sizes: string[];
+//   getAvailableSizes(): string[];
+// }
+
+// let pizza: Pizza;
+
+// function createPizza(name: string, sizes: string[]): Pizza {
+//   return {
+//     name,
+//     sizes,
+//     getAvailableSizes() {
+//       return this.sizes;
+//     },
+//   };
+// }
+
+// pizza = createPizza('Pepperoni', ['small', 'medium']);
+// pizza.getAvailableSizes();
+
+// extending interfaces
