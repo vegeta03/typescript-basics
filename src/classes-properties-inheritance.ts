8dbd14ea836c9ccbabbda635025cc7eec7a4dec7
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-16 01:38:49 +0530

    classes and inheritance

--- src/classes-properties-inheritance.ts
+++ src/classes-properties-inheritance.ts
@@ -57,7 +57,25 @@
 
 //   console.log(pizza);
 
-// setters and getters
+// // setters and getters
+// class Sizes {
+//   constructor(private sizes: Array<string>) {}
+
+//   get availableSizes() {
+//     return this.sizes;
+//   }
+
+//   set availableSizes(sizes: Array<string>) {
+//     this.sizes = sizes;
+//   }
+// }
+
+// const sizes = new Sizes(['small', 'medium']);
+// console.log(sizes.availableSizes);
+// sizes.availableSizes = ['medium', 'large'];
+// console.log(sizes.availableSizes);
+
+// classes and inheritance
 class Sizes {
   constructor(private sizes: Array<string>) {}
 
@@ -70,7 +88,23 @@ class Sizes {
   }
 }
 
-const sizes = new Sizes(['small', 'medium']);
-console.log(sizes.availableSizes);
-sizes.availableSizes = ['medium', 'large'];
-console.log(sizes.availableSizes);
+class Pizza extends Sizes {
+  toppings: string[] = [];
+
+  constructor(readonly name: string, sizes: Array<string>) {
+    super(sizes);
+  }
+
+  addToppings(topping: string): void {
+    this.toppings.push(topping);
+  }
+
+  getName() {
+    return this.name;
+  }
+}
+
+const pizza = new Pizza('Pepperoni', ['small', 'medium']);
+console.log(pizza.availableSizes);
+
+// abstract classes
