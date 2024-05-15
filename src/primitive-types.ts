045a81931a1f016edcb0aabb197763053727fd57
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-15 13:51:29 +0530

    `any` type

--- src/primitive-types.ts
+++ src/primitive-types.ts
@@ -1,36 +1,36 @@
-// printing "Hello, World!!!"
-console.log('Hello, World!!!');
+// // printing "Hello, World!!!"
+// console.log('Hello, World!!!');
 
-let pizzaCost: number = 10;
-const pizzaToppings: number = 2;
+// let pizzaCost: number = 10;
+// const pizzaToppings: number = 2;
 
-// string type and literals
+// // string type and literals
 
-let coupon: string;
-coupon = 'pizza25';
+// let coupon: string;
+// coupon = 'pizza25';
 
-function normalizeCoupon(code: string): string {
-  return code.toUpperCase();
-}
+// function normalizeCoupon(code: string): string {
+//   return code.toUpperCase();
+// }
 
-const code = normalizeCoupon(coupon);
-console.log(code);
+// const code = normalizeCoupon(coupon);
+// console.log(code);
 
-const multiLineString = `
-This is a 
-multi-line string.
-`;
-console.log(multiLineString);
+// const multiLineString = `
+// This is a 
+// multi-line string.
+// `;
+// console.log(multiLineString);
 
-let couponMesage = normalizeCoupon(coupon);
-// ES6 string literal
-couponMesage = `The Final coupon code is ${couponMesage}`;
-console.log(couponMesage);
+// let couponMesage = normalizeCoupon(coupon);
+// // ES6 string literal
+// couponMesage = `The Final coupon code is ${couponMesage}`;
+// console.log(couponMesage);
 
-// boolean
+// // boolean
 
-let isTrue: boolean;
-isTrue = true;
-console.log(isTrue);
-isTrue = !isTrue;
-console.log(isTrue);
+// let isTrue: boolean;
+// isTrue = true;
+// console.log(isTrue);
+// isTrue = !isTrue;
+// console.log(isTrue);
---
+++ src/typescript-types.ts
@@ -0,0 +1,7 @@
+// `any` type
+let coupon;
+coupon = 25;
+coupon = 'pizza25';
+coupon = true;
+
+// Implicit vs Explicit types
