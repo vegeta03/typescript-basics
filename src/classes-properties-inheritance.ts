20a25b2b3adf5a7bc700bd72b6e4a288085ff9de
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-16 10:41:54 +0530

    interface contracts with "implements"

--- src/classes-properties-inheritance.ts
+++ src/classes-properties-inheritance.ts
@@ -75,36 +75,152 @@
 // sizes.availableSizes = ['medium', 'large'];
 // console.log(sizes.availableSizes);
 
-// classes and inheritance
-class Sizes {
-  constructor(private sizes: Array<string>) {}
+// // classes and inheritance
+// class Sizes {
+//   constructor(private sizes: Array<string>) {}
 
-  get availableSizes() {
-    return this.sizes;
-  }
+//   get availableSizes() {
+//     return this.sizes;
+//   }
 
-  set availableSizes(sizes: Array<string>) {
-    this.sizes = sizes;
-  }
-}
+//   set availableSizes(sizes: Array<string>) {
+//     this.sizes = sizes;
+//   }
+// }
 
-class Pizza extends Sizes {
-  toppings: string[] = [];
+// class Pizza extends Sizes {
+//   toppings: string[] = [];
 
-  constructor(readonly name: string, sizes: Array<string>) {
-    super(sizes);
-  }
+//   constructor(readonly name: string, sizes: Array<string>) {
+//     super(sizes);
+//   }
 
-  addToppings(topping: string): void {
-    this.toppings.push(topping);
-  }
+//   addToppings(topping: string): void {
+//     this.toppings.push(topping);
+//   }
 
-  getName() {
-    return this.name;
-  }
-}
+//   getName() {
+//     return this.name;
+//   }
+// }
 
-const pizza = new Pizza('Pepperoni', ['small', 'medium']);
-console.log(pizza.availableSizes);
+// const pizza = new Pizza('Pepperoni', ['small', 'medium']);
+// console.log(pizza.availableSizes);
 
-// abstract classes
+// // abstract classes
+// abstract class Sizes {
+//   constructor(private sizes: Array<string>) {}
+
+//   get availableSizes() {
+//     return this.sizes;
+//   }
+
+//   set availableSizes(sizes: Array<string>) {
+//     this.sizes = sizes;
+//   }
+// }
+
+// class Pizza extends Sizes {
+//   toppings: string[] = [];
+
+//   constructor(readonly name: string, sizes: Array<string>) {
+//     super(sizes);
+//   }
+
+//   addToppings(topping: string): void {
+//     this.toppings.push(topping);
+//   }
+
+//   getName() {
+//     return this.name;
+//   }
+// }
+
+// const pizza = new Pizza('Pepperoni', ['small', 'medium']);
+// console.log(pizza.availableSizes);
+
+// // protected members and inheritance
+// abstract class Sizes {
+//   constructor(protected sizes: Array<string>) {}
+
+//   get availableSizes() {
+//     return this.sizes;
+//   }
+
+//   set availableSizes(sizes: Array<string>) {
+//     this.sizes = sizes;
+//   }
+// }
+
+// class Pizza extends Sizes {
+//   toppings: string[] = [];
+
+//   constructor(readonly name: string, sizes: Array<string>) {
+//     super(sizes);
+//   }
+
+//   updateSizes(sizes: Array<string>) {
+//     this.sizes = sizes;
+//   }
+
+//   addToppings(topping: string): void {
+//     this.toppings.push(topping);
+//   }
+
+//   getName() {
+//     return this.name;
+//   }
+// }
+
+// const pizza = new Pizza('Pepperoni', ['small', 'medium']);
+// console.log(pizza.availableSizes);
+// pizza.updateSizes(['small']);
+// console.log(pizza.availableSizes);
+
+// // interface contracts with "implements"
+// interface SizesInterface {
+//   availableSizes: string[];
+// }
+
+// abstract class Sizes implements SizesInterface {
+//   constructor(protected sizes: Array<string>) {}
+
+//   get availableSizes() {
+//     return this.sizes;
+//   }
+
+//   set availableSizes(sizes: Array<string>) {
+//     this.sizes = sizes;
+//   }
+// }
+
+// interface PizzaInterface {
+//   readonly name: string;
+//   updateSizes(sizes: Array<string>): void;
+//   addToppings(topping: string): void;
+// }
+
+// class Pizza extends Sizes implements PizzaInterface {
+//   toppings: string[] = [];
+
+//   constructor(readonly name: string, sizes: Array<string>) {
+//     super(sizes);
+//   }
+
+//   updateSizes(sizes: Array<string>): void {
+//     this.sizes = sizes;
+//   }
+
+//   addToppings(topping: string): void {
+//     this.toppings.push(topping);
+//   }
+
+//   getName() {
+//     return this.name;
+//   }
+// }
+
+// const pizza = new Pizza('Pepperoni', ['small', 'medium']);
+// console.log(pizza.availableSizes);
+// pizza.updateSizes(['small']);
+// console.log(pizza.availableSizes);
