a0fc9bb746a59ce284e4287e41519af14d4d49b7
Shyam Sreenivasan <shyam.vegeta@gmail.com>
2024-05-16 11:50:15 +0530

    static properties and methods

--- src/classes-properties-inheritance.ts
+++ src/classes-properties-inheritance.ts
@@ -224,3 +224,15 @@
 // console.log(pizza.availableSizes);
 // pizza.updateSizes(['small']);
 // console.log(pizza.availableSizes);
+
+// // static properties and methods
+
+// class Coupon {
+//   static allowed = ['Pepperoni', 'Blazing Inferno'];
+//   static create(percentage: number): string {
+//     return `PIZZA_RESTAURANT_${percentage}`;
+//   }
+// }
+
+// console.log(Coupon.allowed);
+// console.log(Coupon.create(25));
